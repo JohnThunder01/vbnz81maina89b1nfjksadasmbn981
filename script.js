168c071e20a79ce963e57f6cc37e1d6d11552691
John <109166944+JohnThunder01@users.noreply.github.com>
2024-05-20 17:37:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ const menu = document.getElementById('menu');
 
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('open');
-  menu.classList.toggle('close');
 }); 
 
 const LogButton = document.getElementById('LogButton');
 const LogPage = document.getElementById('LogPage');
+const LogPageX = document.getElementById('LogPageX');
 
 LogButton.addEventListener('click', () => {
   LogPage.classList.toggle('open');
-  LogPage.classList.toggle('close');
+});
+
+LogPageX.addEventListener('click', () => {
+  LogPage.classList.remove('open');
+});
+
+// JavaScript code for login functionality
+const form = document.getElementById('login-form');
+const errorMessage = document.getElementById('error-message');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Perform login validation and authentication here
+    if (username === 'admin' && password === 'password') {
+        // Successful login, redirect to another page or perform other actions
+        window.location.href = 'dashboard.html';
+    } else {
+        errorMessage.textContent = 'Invalid username or password.';
+    }
 });
